def45c84012ce81b10d96187bc1f08fb50264f2a
kjetilnym0en <kjetilsn@uia.no>
2023-02-22 11:38:16 +0100

    Endret main.go

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 		celsius = conv.KelvinToCelsius(inputTemp)
 	}
 
-	// Konverterer Celsius til output temperatur
+	// Konverterer input temperatur til output temperatur
 	switch out {
 	case "C":
 		fmt.Printf("%.2f°C\n", celsius)
@@ -62,152 +62,6 @@ func main() {
 
 // Funksjonen sjekker om flagget er spesifisert på kommandolinje
 func isFlagPassed(name string) bool {
-	var found bool
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
-}
-
-/*
-
-// Definerer flag-variablene i hoved-"scope"
-var fahr float64
-var celsius float64
-var kelvin float64
-var out string
-
-// Bruker init (som anbefalt i dokumentasjonen) for å sikre at flagvariablene
-// er initialisert.
-func init() {
-
-
-	// Definerer og initialiserer flagg-variablene
-	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader fahrenheit")
-	flag.Float64Var(&celsius, "C", 0.0, "temperatur i grader fahrenheit")
-	flag.Float64Var(&kelvin, "K", 0.0, "temperatur i grader fahrenheit")
-	// Du må selv definere flag-variablene for "C" og "K"
-	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
-	// Du må selv definere flag-variabelen for -t flagget, som bestemmer
-	// hvilken temperaturskala skal brukes når funfacts skal vises
-}
-
-func main() {
-    var fromTemp float64
-    var fromUnit string
-    var toUnit string
-
-    // Legg til flagg for inputverdi, inputenhet og ønsket utenheter
-    flag.Float64Var(&fromTemp, "temp", 0.0, "input temperature")
-    flag.StringVar(&fromUnit, "from", "C", "input unit [C, F, K]")
-    flag.StringVar(&toUnit, "to", "C", "output unit [C, F, K]")
-    flag.Parse()
-// Velg konverteringsfunksjon basert på input- og utenheter
-    var conversionFunc func(float64) float64
-    switch fromUnit {
-    case "C":
-        switch toUnit {
-        case "F":
-            conversionFunc = conv.CelsiusToFahrenheit
-        case "K":
-            conversionFunc = conv.CelsiusToKelvin
-        default:
-            conversionFunc = func(x float64) float64 { return x }
-        }
-    case "F":
-        switch toUnit {
-        case "C":
-            conversionFunc = conv.FahrenheitToCelsius
-        case "K":
-            conversionFunc = conv.FahrenheitToKelvin
-        default:
-            conversionFunc = func(x float64) float64 { return x }
-        }
-    case "K":
-        switch toUnit {
-        case "C":
-            conversionFunc = conv.KelvinToCelsius
-        case "F":
-            conversionFunc = conv.KelvinToFahrenheit
-        default:
-            conversionFunc = func(x float64) float64 { return x }
-        }
-    default:
-        conversionFunc = func(x float64) float64 { return x }
-    }
-
-    // Utfør konvertering
-    toTemp := conversionFunc(fromTemp)
-
-    // Skriv ut resultatet
-    fmt.Printf("Input verdi %.2f %s er konvertert til %.2f %s\n", fromTemp, fromUnit, toTemp, toUnit)
-}
-
-
-
-/*
-func convertTemp(temp float64, from string, to string) (float64, error) {
-    switch from {
-    case "F":
-        switch to {
-        case "C":
-            return conv.FahrenheitToCelsius(temp), nil
-        case "K":
-            return conv.FahrenheitToKelvin(temp), nil
-        default:
-            return 0, fmt.Errorf("invalid to scale: %s", to)
-        }
-    case "C":
-        switch to {
-        case "F":
-            return conv.CelsiusToFahrenheit(temp), nil
-
-
-        case "K":
-            return conv.CelsiusToKelvin(temp), nil
-        default:
-            return 0, fmt.Errorf("invalid to scale: %s", to)
-        }
-    case "K":
-        switch to {
-        case "F":
-            return conv.KelvinToFahrenheit(temp), nil
-        case "C":
-            return conv.KelvinToCelsius(temp), nil
-        default:
-            return 0, fmt.Errorf("invalid to scale: %s", to)
-        }
-    default:
-    fmt.Printf("Input verdi %.2f %s er konvertert til %.2f %s\n", fromTemp, fromUnit, toTemp, toUnit)}
-}
-
-// Bruk pakken conv til å vise temperaturkonverteringen som string
-func formatTempConversion(temp float64, from string, to string) string {
-    converted, _ := convertTemp(temp, from, to)
-    return fmt.Sprintf("%g%s er %g%s", temp, from, converted, to)
-}
-
-// Bruk pakken conv til å konvertere temperaturer
-// funksjonen vil konvertere temperaturen fra F, C eller K til alle de tre andre
-func convertAllTempScales(temp float64, from string) (float64, float64, error) {
-    switch from {
-    case "F":
-        return conv.FahrenheitToCelsius(temp), conv.FahrenheitToKelvin(temp), nil
-    case "C":
-        return conv.CelsiusToFahrenheit(temp), conv.CelsiusToKelvin(temp), nil
-    case "K":
-        return conv.KelvinToFahrenheit(temp), conv.KelvinToCelsius(temp), nil
-    default:
-        return 0, 0, fmt.Errorf("invalid from scale: %s", from)
-    }
-}
-
-
-// Funksjonen sjekker om flagget er spesifisert på kommandolinje
-// Du trenger ikke å bruke den, men den kan hjelpe med logikken
-func isFlagPassed(name string) bool {
 	found := false
 	flag.Visit(func(f *flag.Flag) {
 		if f.Name == name {
@@ -216,4 +70,3 @@ func isFlagPassed(name string) bool {
 	})
 	return found
 }
-*/
